0ddc797658c51c8b4d2f05e5f62990a5f031c11e
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2023-11-08 14:01:11 +0600

    POST Method created successfully

--- index.js
+++ index.js
@@ -29,6 +29,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const assignmentCollections = client.db('learniverse').collection('allAssignments');
+
+  //Posting New Assignments to the DataBase 
+    app.post('/assignments', async(req,res)=>{
+      const newAssignments = req.body;
+      console.log(newAssignments);
+      const result = await assignmentCollections.insertOne(newAssignments);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
